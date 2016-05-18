85725668bb5096272610b0bc639efe89b1e4aa75
Dusty Mabe <dusty@dustymabe.com>
2016-05-18 17:05:36 -0400

    Updates for lab3

--- vagrantcdk/answers/complete-lab3.sh
+++ vagrantcdk/answers/complete-lab3.sh
@@ -7,7 +7,6 @@ cd ~/labs/lab3
 docker stop bigapp #a little safer then $(docker ps -ql)
 docker rm bigapp #a little safer then $(docker ps -ql)
 
-    
 sudo mkdir -p /var/lib/mariadb
 sudo mkdir -p /var/lib/wp_uploads
 sudo chcon -Rt svirt_sandbox_file_t /var/lib/mariadb
@@ -16,14 +15,9 @@ sudo chcon -Rt svirt_sandbox_file_t /var/lib/wp_uploads
 docker build -t mariadb -f mariadb/Dockerfile.reference mariadb/.
 docker build -t wordpress -f wordpress/Dockerfile.reference wordpress/.
 
-echo -e '\nLABEL RUN docker run -d -v /var/lib/mysql:/var/lib/mysql  --name NAME -e DBUSER=${DBUSER} -e DBPASS={$DBPASS} -e DBNAME=${DBNAME} -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> mariadb/Dockerfile.reference
-
-echo -e '\nLABEL RUN docker run -d -v /var/lib/wp_uploads:/var/www/html/wp-content/uploads -p 80:80 --link=mariadb:db --name NAME -e NAME=NAME -e IMAGE=IMAGE IMAGE' >> wordpress/Dockerfile.reference
-
 docker build -t mariadb -f mariadb/Dockerfile.reference mariadb/.
 docker build -t wordpress -f wordpress/Dockerfile.reference wordpress/.
 
-#TARGET_IP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 TARGET_IP='dev.example.com'
 
 docker tag -f mariadb $TARGET_IP:5000/mariadb
