e39ea12a1262f759283c66800ad9e6574b415caa
Dusty Mabe <dusty@dustymabe.com>
2016-05-18 13:46:24 -0400

    Add some missing items from lab1 answers script.

--- vagrantcdk/answers/complete-lab1.sh
+++ vagrantcdk/answers/complete-lab1.sh
@@ -4,5 +4,10 @@ set -eux
 
 cd ~/labs/lab1
 
+docker build -t redhat/apache .
+docker run -dt -p 80:80 --name apache redhat/apache
+docker exec apache yum -y install iproute
+docker rm -f apache
+
 docker build -t registry registry/
 docker run --restart="always" --name registry -p 5000:5000 -d registry
