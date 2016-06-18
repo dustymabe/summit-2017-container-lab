d952dfb5fe6b88f5037eed7222713d1edd61bd42
Dusty Mabe <dusty@dustymabe.com>
2016-06-18 08:28:25 -0400

    answers: Add in a lab5 answers script.

---
+++ labs/answers/complete-lab5.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#execute the functionality in lab5
+
+mkdir -p ~/workspace/nulecule/
+cp -R ~/labs/lab5/* ~/workspace/nulecule/
+
+# Fix nulecule file
+sed -i 's|#   source: "docker://mariadb-rhel7-atomicapp"|    source: "docker://mariadb-rhel7-atomicapp" |' ~/workspace/nulecule/wordpress-atomicapp/Nulecule
+
+# Log back in to CDK openshift
+oc login --insecure-skip-tls-verify=true -u openshift-dev -p devel localhost:8443
+
+# Clean up items from previous lab
+oc delete project sample-project
+sleep 10
+oc new-project sample-project
+
+# Inspect atomicapp docker image
+docker pull devstudio/atomicapp:0.5.0
+sudo atomic info devstudio/atomicapp:0.5.0
+
+# Generate Answers file
+sudo -E atomic run devstudio/atomicapp:0.5.0 --mode genanswers ./
+
+# Populate the contents of the answers file
+CONTENTS="
+[mariadb]
+image = rhel-cdk.example.com:5000/mariadb
+db_pass = password
+db_user = user
+db_name = name
+[wordpress]
+image = rhel-cdk.example.com:5000/wordpress
+db_pass = password
+db_user = user
+db_name = name
+[general]
+namespace = sample-project
+provider = openshift
+provider-config = /home/vagrant/.kube/config
+"
+sudo -E su -c 'echo "$CONTENTS" > answers.conf'
+
+# Run the Atomic App
+sudo -E atomic run devstudio/atomicapp:0.5.0 ./
+
+
+# Build the Atomic App
+docker build -t wordpress-rhel7-atomicapp ./
+
+# Tag+Push
+docker tag wordpress-rhel7-atomicapp rhel-cdk.example.com:5000/wordpress-rhel7-atomicapp
+docker push rhel-cdk.example.com:5000/wordpress-rhel7-atomicapp
